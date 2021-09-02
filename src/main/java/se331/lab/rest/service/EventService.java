f24c23154b5000c28366b9e1b7d4fa380efa73e3
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-02 18:18:27 +0700

    Initial commit

---
+++ src/main/java/se331/lab/rest/service/EventService.java
@@ -0,0 +1,12 @@
+package se331.lab.rest.service;
+
+import se331.lab.rest.entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+    Integer getEventSize();
+    List<Event> getEvents(Integer pageSize,Integer page);
+    Event getEvent(Long id);
+
+}
---
+++ src/main/java/se331/lab/rest/service/EventServiceImpl.java
@@ -0,0 +1,28 @@
+package se331.lab.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se331.lab.rest.dao.EventDao;
+import se331.lab.rest.entity.Event;
+
+import java.util.List;
+
+@Service
+public class EventServiceImpl implements EventService{
+    @Autowired
+    EventDao eventDao;
+    @Override
+    public Integer getEventSize() {
+        return eventDao.getEventSize();
+    }
+
+    @Override
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        return eventDao.getEvents(pageSize, page);
+    }
+
+    @Override
+    public Event getEvent(Long id) {
+        return eventDao.getEvent(id);
+    }
+}
